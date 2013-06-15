accac371ec1e623d9cc69631718db1269567f499
Chris <chris.boesch@hotmail.com>
2013-06-15 13:30:26 +0200

    doppelter Code: address/books/invoice/random wird in setUp initialisiert. setCustomer auch in setUp, da ueberall der gleiche Customer verwendet wird. Nutzen: Init in einer Methode und viel uebersichtlicher.

--- src/test/java/bsp4_1/TestsToRefactor.java
+++ src/test/java/bsp4_1/TestsToRefactor.java
@@ -16,232 +16,207 @@ import bsp4_1.model.Customer;
 import bsp4_1.model.Invoice;
 import bsp4_1.model.Item;
 import bsp4_1.model.ToMuchItemsException;
+import org.junit.Before;
 
 public class TestsToRefactor {
 
-	@Test
-	public void test_addSomeBooks() {
+    private Book b1, b2, b3, b4, b5;
+    Invoice i;
+    Address a;
+    Customer c;
+    Random r;
 
-		Address a = null;
-		Customer c = null;
-		final int DELIVERY_COSTS_5 = 7;
-		final int DELIVERY_COSTS_10 = 5;
-		Book b1 = new Book(12.5, "das leben des max mustermann", "mustermann", 125);
-		Book b2 = new Book(14.99, "sofies welt", "gaarder", 251);
-		Book b3 = new Book(8.69, "the jungle books", "kipling", 215);
-		Book b4 = new Book(15.99, "great expectations", "dickens", 314);
-		Book b5 = new Book(12.99, "kÃ¼sschen, kÃ¼sschen", "dahl", 145);
-		int maxItemOrder = 10;
+    @Before
+    public void setUp() {
+        a = new Address();
+        c = null;
+        b1 = new Book(12.5, "das leben des max mustermann", "mustermann", 125);
+        b2 = new Book(14.99, "sofies welt", "gaarder", 251);
+        b3 = new Book(8.69, "the jungle books", "kipling", 215);
+        b4 = new Book(15.99, "great expectations", "dickens", 314);
+        b5 = new Book(12.99, "küsschen, küsschen", "dahl", 145);
+        i = null;
+        c = new Customer(a, "franz", "beispiel");
+        i = new Invoice();
+        i.setCustomer(c);
+        r = new Random();
+    }
 
-		Invoice i = null;
-		int amount = 0;
+    @Test
+    public void test_addSomeBooks() {
 
-		try {
-			a = new Address();
-			c = new Customer(a, "franz", "beispiel");
-			i = new Invoice();
-			i.setCustomer(c);
-			i.setMaxItemOrder(maxItemOrder);
+        final int DELIVERY_COSTS_5 = 7;
+        final int DELIVERY_COSTS_10 = 5;
 
-			Random r = new Random();
-			amount = r.nextInt(15);
+        int maxItemOrder = 10;
 
-			i.addItems(b1, amount);
+        int amount = 0;
 
-			assertEquals(i.getItemCount(), amount);
-			assertEquals(i.getCustomer().getFirstName(), "franz");
-			assertEquals(i.getCustomer().getLastName(), "beispiel");
-			assertEquals(i.getCustomer().getAddress(), a);
-			assertEquals(i.getMaxItemOrder(), maxItemOrder);
+        try {
 
-		} catch (Exception e) {
-			if (amount < 10) {
-				assertFalse("dieser fehler hÃ¤tte nicht auftreten sollen", true);
-			} else {
-				assertTrue("es wurden zuviele Items hinzugefÃ¼gt", true);
-			}
-		} finally {
-			a = null;
-			c = null;
-			b1 = null;
-			b2 = null;
-			b3 = null;
-			b4 = null;
-			b5 = null;
-			i = null;
-			amount = 0;
-		}
+            i.setMaxItemOrder(maxItemOrder);
+            amount = r.nextInt(15);
 
-	}
+            i.addItems(b1, amount);
 
-	@Test
-	public void test_addManyItems_getTotalPrice() {
+            assertEquals(i.getItemCount(), amount);
+            assertEquals(i.getCustomer().getFirstName(), "franz");
+            assertEquals(i.getCustomer().getLastName(), "beispiel");
+            assertEquals(i.getCustomer().getAddress(), a);
+            assertEquals(i.getMaxItemOrder(), maxItemOrder);
 
-		final int DELIVERY_COSTS_5 = 7;
-		final int DELIVERY_COSTS_10 = 5;
-		Address a = null;
-		Customer c = null;
-		int maxItemOrder = 10;
-		Book b1 = new Book(12.5, "das leben des max mustermann", "mustermann", 125);
-		Book b2 = new Book(14.99, "sofies welt", "gaarder", 251);
-		Book b3 = new Book(8.69, "the jungle books", "kipling", 215);
-		Book b4 = new Book(15.99, "great expectations", "dickens", 314);
-		Book b5 = new Book(12.99, "kÃ¼sschen, kÃ¼sschen", "dahl", 145);
+        } catch (Exception e) {
+            if (amount < 10) {
+                assertFalse("dieser fehler hätte nicht auftreten sollen", true);
+            } else {
+                assertTrue("es wurden zuviele Items hinzugefügt", true);
+            }
+        } finally {
+            a = null;
+            c = null;
+            b1 = null;
+            b2 = null;
+            b3 = null;
+            b4 = null;
+            b5 = null;
+            i = null;
+            amount = 0;
+        }
 
-		List<Item> bookList = new ArrayList<Item>();
-		bookList.add(b1);
-		bookList.add(b2);
-		bookList.add(b3);
-		bookList.add(b4);
-		bookList.add(b5);
+    }
 
-		Invoice i = null;
-		int amount = 0;
+    @Test
+    public void test_addManyItems_getTotalPrice() {
 
-		try {
-			a = new Address();
-			c = new Customer(a, "franz", "beispiel");
+        final int DELIVERY_COSTS_5 = 7;
+        final int DELIVERY_COSTS_10 = 5;
+        int maxItemOrder = 10;
 
-			i = new Invoice();
-			i.setCustomer(c);
-			i.setMaxItemOrder(maxItemOrder);
+        List<Item> bookList = new ArrayList<Item>();
+        bookList.add(b1);
+        bookList.add(b2);
+        bookList.add(b3);
+        bookList.add(b4);
+        bookList.add(b5);
 
-			Random r = new Random();
-			amount = r.nextInt(5) + 1;
-			double totalPrice = 0;
+        int amount = 0;
 
-			for (Item item : bookList) {
-				i.addItems(item, amount);
+        try {
+            i.setMaxItemOrder(maxItemOrder);
 
-				if (amount < 5)
-					totalPrice += amount * item.getPrice() + DELIVERY_COSTS_5;
-				else
-					totalPrice += amount * item.getPrice() + DELIVERY_COSTS_10;
-			}
+            amount = r.nextInt(5) + 1;
+            double totalPrice = 0;
 
-			if (totalPrice == i.getTotalPrice()) {
-				assertTrue(true);
-			}
+            for (Item item : bookList) {
+                i.addItems(item, amount);
 
-		} catch (Exception e) {
-			if (amount < 10) {
-				assertFalse("dieser fehler hÃ¤tte nicht auftreten dÃ¼rfen", true);
-			} else {
-				assertTrue("es wurden zuviele Items hinzugefÃ¼gt", true);
-			}
-		} finally {
-			a = null;
-			c = null;
-			b1 = null;
-			b2 = null;
-			b3 = null;
-			b4 = null;
-			b5 = null;
-			i = null;
-			amount = 0;
-		}
-	}
+                if (amount < 5) {
+                    totalPrice += amount * item.getPrice() + DELIVERY_COSTS_5;
+                } else {
+                    totalPrice += amount * item.getPrice() + DELIVERY_COSTS_10;
+                }
+            }
 
-	@Test
-	public void test_itemCount_changeMaxItemOrder() {
+            if (totalPrice == i.getTotalPrice()) {
+                assertTrue(true);
+            }
 
-		final int DELIVERY_COSTS_5 = 7;
-		final int DELIVERY_COSTS_10 = 5;
-		Address a = null;
-		Customer c = null;
-		Book b1 = new Book(12.5, "das leben des max mustermann", "mustermann", 125);
-		Book b2 = new Book(14.99, "sofies welt", "gaarder", 251);
-		Book b3 = new Book(8.69, "the jungle books", "kipling", 215);
-		Book b4 = new Book(15.99, "great expectations", "dickens", 314);
-		Book b5 = new Book(12.99, "kÃ¼sschen, kÃ¼sschen", "dahl", 145);
-		int maxItemOrder = 150;
+        } catch (Exception e) {
+            if (amount < 10) {
+                assertFalse("dieser fehler hätte nicht auftreten dürfen", true);
+            } else {
+                assertTrue("es wurden zuviele Items hinzugefügt", true);
+            }
+        } finally {
+            a = null;
+            c = null;
+            b1 = null;
+            b2 = null;
+            b3 = null;
+            b4 = null;
+            b5 = null;
+            i = null;
+            amount = 0;
+        }
+    }
 
-		List<Item> bookList = new ArrayList<Item>();
-		bookList.add(b1);
-		bookList.add(b2);
-		bookList.add(b3);
-		bookList.add(b4);
-		bookList.add(b5);
+    @Test
+    public void test_itemCount_changeMaxItemOrder() {
 
-		Invoice i = null;
+        final int DELIVERY_COSTS_5 = 7;
+        final int DELIVERY_COSTS_10 = 5;
 
-		int amount = 0;
+        int maxItemOrder = 150;
 
-		try {
-			a = new Address();
-			c = new Customer(a, "franz", "beispiel");
+        List<Item> bookList = new ArrayList<Item>();
+        bookList.add(b1);
+        bookList.add(b2);
+        bookList.add(b3);
+        bookList.add(b4);
+        bookList.add(b5);
 
-			i = new Invoice();
-			i.setCustomer(c);
-			i.setMaxItemOrder(maxItemOrder);
+        int amount = 0;
 
-			Random r = new Random();
-			amount = r.nextInt(30) + 1;
-			int itemCount = 0;
-			for (Item item : bookList) {
-				i.addItems(item, amount);
-				itemCount += amount;
-			}
+        try {
 
-			if (itemCount == i.getItemCount()) {
-				assertTrue(true);
-			} else {
-				assertFalse(true);
-			}
+            i.setMaxItemOrder(maxItemOrder);
 
-		} catch (Exception e) {
-			assertFalse(true);
-		} finally {
-			a = null;
-			c = null;
-			b1 = null;
-			b2 = null;
-			b3 = null;
-			b4 = null;
-			b5 = null;
-			i = null;
-			amount = 0;
-		}
-	}
+            amount = r.nextInt(30) + 1;
+            int itemCount = 0;
+            for (Item item : bookList) {
+                i.addItems(item, amount);
+                itemCount += amount;
+            }
 
-	@Test(expected = Exception.class)
-	public void test_addToManyItems() throws ToMuchItemsException {
+            if (itemCount == i.getItemCount()) {
+                assertTrue(true);
+            } else {
+                assertFalse(true);
+            }
 
-		final int DELIVERY_COSTS_5 = 7;
-		final int DELIVERY_COSTS_10 = 5;
-		Address a = null;
-		Customer c = null;
-		Book b1 = new Book(12.5, "das leben des max mustermann", "mustermann", 125);
-		Book b2 = new Book(14.99, "sofies welt", "gaarder", 251);
-		Book b3 = new Book(8.69, "the jungle books", "kipling", 215);
-		Book b4 = new Book(15.99, "great expectations", "dickens", 314);
-		Book b5 = new Book(12.99, "kÃ¼sschen, kÃ¼sschen", "dahl", 145);
-		int maxItemOrder = 0;
+        } catch (Exception e) {
+            assertFalse(true);
+        } finally {
+            a = null;
+            c = null;
+            b1 = null;
+            b2 = null;
+            b3 = null;
+            b4 = null;
+            b5 = null;
+            i = null;
+            amount = 0;
+        }
+    }
 
-		Invoice i = null;
+    @Test(expected = Exception.class)
+    public void test_addToManyItems() throws ToMuchItemsException {
 
-		int amount = 1;
+        final int DELIVERY_COSTS_5 = 7;
+        final int DELIVERY_COSTS_10 = 5;
 
-		try {
-			a = new Address();
-			c = new Customer(a, "franz", "beispiel");
+        int maxItemOrder = 0;
 
-			i = new Invoice();
-			i.setCustomer(c);
-			i.setMaxItemOrder(maxItemOrder);
+        Invoice i = null;
 
-			i.addItems(b1, amount);
+        int amount = 1;
 
-		} finally {
-			a = null;
-			c = null;
-			b1 = null;
-			b2 = null;
-			b3 = null;
-			b4 = null;
-			b5 = null;
-			i = null;
-			amount = 0;
-		}
-	}
+        try {
+
+            i.setMaxItemOrder(maxItemOrder);
+
+            i.addItems(b1, amount);
+
+        } finally {
+            a = null;
+            c = null;
+            b1 = null;
+            b2 = null;
+            b3 = null;
+            b4 = null;
+            b5 = null;
+            i = null;
+            amount = 0;
+        }
+    }
 }
